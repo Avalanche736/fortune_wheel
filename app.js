2f43aeaed6f7a73b89743cd3d92522209af406a6
Avalanche <dposyagin@gmail.com>
2025-01-15 14:21:06 +0300

    test: user amount

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ xhr1.responseType = "json";
 xhr1.onload = () => {
     if (xhr1.readyState == 4 && xhr.status == 200) {
         console.log("get_fortune_wheel_amount");
-        console.log(xhr1.responseText);
+        console.log(xhr1.response);
     } else {
         console.log(`Error: ${xhr1.status}`);
     }
